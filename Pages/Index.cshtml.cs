97708469fe508f12dd1aeefbbd882c9f22e1bb0c
Wojciech Chodakowski <chodakowski.wojtek@gmail.com>
2022-10-18 19:50:01 +0200

    first commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Page_Of_Everything.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+
+        }
+
+
+        [BindProperty]
+        public static string? Name { get; set; } 
+
+        public void OnGet()
+        {
+            Name = "To the Page Of Everything";
+        }
+
+        public void OnPost()
+        {
+
+        }
+    }
+}
